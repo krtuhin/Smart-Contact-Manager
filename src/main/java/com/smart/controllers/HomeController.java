ab31835c32b4f9921adca9c72226c0c2e8086678
krtuhin <tuhin.km2774@gmail.com>
2023-08-04 15:45:37 +0530

    Setup Bootstrap & Templates

--- src/main/java/com/smart/controllers/HomeController.java
+++ src/main/java/com/smart/controllers/HomeController.java
@@ -3,6 +3,8 @@ package com.smart.controllers;
 import com.smart.dao.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
@@ -10,5 +12,23 @@ public class HomeController {
     @Autowired
     private UserRepository userRepository;
 
+    //handler for home page
+    @GetMapping("/home")
+    public String home(Model m) {
+
+        m.addAttribute("title", "Home - Smart Contact Manager");
+
+        return "home";
+
+    }
+
+    //handler for about page
+    @GetMapping("/about")
+    public String about(Model m) {
+
+        m.addAttribute("title", "About - Smart Contact Manager");
+
+        return "about";
+    }
 
 }
