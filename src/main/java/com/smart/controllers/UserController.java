d0c5212007e3089ad62c50bd4fd9db3e22ca4bb0
krtuhin <tuhin.km2774@gmail.com>
2023-08-06 18:03:58 +0530

    User Dashboard (get username by Principal)

--- src/main/java/com/smart/controllers/UserController.java
+++ src/main/java/com/smart/controllers/UserController.java
@@ -1,16 +1,38 @@
 package com.smart.controllers;
 
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     //handler for user dashboard
     @GetMapping("/index")
-    public String dashboard() {
+    public String dashboard(Model model, Principal principal) {
+
+        //fetching username from spring security
+        String userName = principal.getName();
+        System.out.println(userName);
+
+        //fetching user from database using username (email)
+        User user = this.userRepository.getUserByUserName(userName);
+        System.out.println(user);
+
+        //sending data from controller to view
+        model.addAttribute("user", user);
+        model.addAttribute("title", "Dashboard - Smart Contact Manager");
+
         return "normal/user_dashboard";
     }
 }
