b90bc040beaf7853ab1c0a1d812b673df27c86d8
krtuhin <tuhin.km2774@gmail.com>
2023-08-07 00:48:22 +0530

    Responsive Sidebar

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,19 @@
-// alert("hello ");
+// sidebar show and hide
+const toggleSidebar = () => {
+
+    if ($(".sidebar").is(":visible")) {
+
+        // hide
+        $(".sidebar").css("display", "none");
+        $(".content").css("margin-left", "0%");
+        $(".barBtn").css("display", "block");
+
+    } else {
+
+        // show
+        $(".sidebar").css("display", "block");
+        $(".content").css("margin-left", "20%");
+        $(".barBtn").css("display", "none");
+
+    }
+};
